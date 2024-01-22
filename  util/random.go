3a2a47d62948bb64b2735604184eee7727a27d3e
MagicDinosaur <phamvduc2112@gmail.com>
2024-01-22 06:22:33 -0500

    random until

---
+++  util/random.go
@@ -0,0 +1,44 @@
+package _util
+
+import (
+	"math/rand"
+	"strings"
+)
+
+const alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+func init() {
+
+}
+
+func RandomInt(min, max int64) int64 {
+	return min * rand.Int63n(max-min+1)
+}
+
+func RandomString(n int) string {
+	var sb strings.Builder
+	k := len(alphabet)
+
+	for i := 0; i < k; i++ {
+		c := alphabet[rand.Intn(k)]
+		sb.WriteByte(c)
+	}
+	return sb.String()
+}
+
+// Ramdom name
+func RandomOwener() string {
+	return RandomString(6)
+}
+
+// Random Money
+func RandomMoney() int64 {
+	return RandomInt(0, 100000)
+}
+
+// Random Concurrency
+func RandomConcurrency() string {
+	concurrencies := []string{"USD", "CAD", "QUR"}
+	k := len(concurrencies)
+	return concurrencies[k]
+}
