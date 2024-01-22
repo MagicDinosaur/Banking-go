160b0a6ff3f93aed2da28f72a667b56bef47da55
MagicDinosaur <phamvduc2112@gmail.com>
2024-01-22 06:03:30 -0500

    create account test unit

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -5,13 +5,15 @@ import (
 	"log"
 	"os"
 	"testing"
+	//auto remove if we dont have _ underscore because we dont use the package directly
+	_ "github.com/lib/pq"
 )
 
 var testQueries *Queries
 
 const (
 	dbDriver = "postgres"
-	dbSource = "postgresql://postgres:postgres@localhost:5432/simple_bank?sslmode=disable"
+	dbSource = "postgresql://root:1234@localhost:5432/gobanking?sslmode=disable"
 )
 
 // entry testpoint for all golang tests
@@ -20,7 +22,9 @@ func TestMain(m *testing.M) {
 	if err != nil {
 		log.Fatal("cannot connect to db: ", err)
 	}
+
 	testQueries = New(conn)
+
 	os.Exit(m.Run())
 
 }
